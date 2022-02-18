00c075385c784a18b6f6b916a3d26861d57242df
Tarik Ualit <tarik@wearefrank.nl>
2022-02-18 15:19:13 +0100

    added last 2 functions

--- project_blockchain/L01 Linked List/HW/LinkedList.py
+++ project_blockchain/L01 Linked List/HW/LinkedList.py
@@ -15,8 +15,8 @@ class LinkedList:
         if self.head == None:
             self.head = Node(new_data)
             self.tail = self.head
-           
             return
+            
         else:
             old_head = self.head
             self.head = Node(new_data)
@@ -66,13 +66,38 @@ class LinkedList:
             current_node = current_node.next
             count += 1
         
-    # Search an element
+    # Search an element, index is 0 based
+
     def find(self, key):
-        return -1
+        if self.head == None:
+            return -1
+        if self.head.data == key:
+            return 0
+        count = 0
+        current_node = self.head
+        while current_node.data != key:
+            count += 1 
+            current_node = current_node.next
+        if current_node.data == key:
+                return count
+        return 99
+
 
     # Sort the linked list
     def sort(self, head):
-        pass
+       valuesArray = []
+       current_node = head
+       while current_node != None:
+           valuesArray.append(current_node.data)
+           current_node = current_node.next
+       valuesArray.sort()
+       self.head = Node(valuesArray[0])
+
+       for val in valuesArray[1:len(valuesArray)-1]:
+           current_node = self.head
+           while current_node.next != None:
+               current_node = current_node.next
+           current_node.next = Node(val)
 
      # Print the linked list
     def printList(self):
