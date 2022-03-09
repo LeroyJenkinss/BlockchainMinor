27699729ea450f6d2133b2413bd750ba429f2703
Tarik <thh.ualit@gmail.com>
2022-03-09 18:15:08 +0100

    added in class work

--- project_blockchain/L01 Linked List/HW/lesson3/EX/Blockchain.py
+++ project_blockchain/L01 Linked List/HW/lesson3/EX/Blockchain.py
@@ -1,10 +1,27 @@
+from hashlib import blake2b
+import hashlib
+import string 
+
 class CBlock:
-    previousHash = None
-    previousBlock = None
-    data = None
     
-    def __init__(self, data, previousBlock):
-        pass        
+    def __init__(self, data=None, previousBlock=None, hashsize=1, previousHash=None):
+        self.data = data
+        self.previousBlock = previousBlock
+        self.hashsize = hashsize
+        if self.previousBlock != None and self.previousBlock.data != None:
+            if isinstance(self.previousBlock.data, str):
+                self.previousHash = hashlib.sha256(self.previousBlock.data.encode()).hexdigest()
+            elif isinstance(self.previousBlock.data, bytes):
+                decodeData = self.previousBlock.data.decode('UTF-8')
+                self.previousHash = hashlib.sha256(decodeData.encode('UTF-8')).hexdigest()
+            elif isinstance(self.previousBlock.data, int):
+                strInt = str(self.previousBlock.data)
+                self.previousHash = hashlib.sha256(strInt.encode()).hexdigest()
+            elif isinstance(self.previousBlock.data, object):
+                self.previousHash = hash(self.previousBlock.data)     
     
     def computeHash(self):
-        return 'aaa'
+        if  isinstance(self.data, str):
+            return hashlib.sha256(self.data.encode('UTF-8')).hexdigest()
+        elif isinstance(self.data, bytes):
+            return hashlib.sha256(self.data).hexdigest()
